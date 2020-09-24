bc210569cd2bd8496d71f8ec747362e68bf528e9
MacPherson <maxim.troitsky@gmail.com>
2020-09-24 20:31:46 +0300

    init

---
+++ Web.Api.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Web.Api.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Web.Api.Infrastrcture/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Web.Api.Infrastrcture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
